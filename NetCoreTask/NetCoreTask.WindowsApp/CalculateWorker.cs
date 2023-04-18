236fc30abd420504286c1f7c25a3d4684c10236c
halilonuryazgan86 <halil.yazgan@gmail.com>
2023-04-18 16:58:49 +0300

    NetCoreTask Düzenleme

---
+++ NetCoreTask/NetCoreTask.WindowsApp/CalculateWorker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreTask.WindowsApp
+{
+    public class CalculateWorker
+    {
+        private List<Condition> conditions = new List<Condition>
+        {
+            new Condition{ MinValue = int.MinValue, MaxValue = 5, ResultValue = 50, ConditionRule = ConditionRule.Normal }, //[-∞, ...., 5]
+            new Condition{ MinValue = 5,  MaxValue= 6, ResultValue = 60, ConditionRule = ConditionRule.EqualToMinAndMax }, //[5,6]
+            new Condition{ MinValue = 10, MaxValue= 14, ResultValue = 70, ConditionRule = ConditionRule.EqualToMax }, //[11,12,13,14]
+            new Condition{ MinValue = 14, MaxValue= 20, ResultValue = 90, ConditionRule = ConditionRule.Normal }, //[15,16,17,18,19]
+            new Condition{ MinValue = 20, MaxValue= int.MaxValue, ResultValue = 100, ConditionRule = ConditionRule.EqualToMin } //[20, ..., ∞]
+        };
+
+        private Condition CalculateCondition(Condition condition, int input)
+        {
+            switch (condition.ConditionRule)
+            {
+                case ConditionRule.Normal:
+                    if (condition.MinValue < input && condition.MaxValue > input)
+                        return condition;
+                    break;
+                case ConditionRule.EqualToMinAndMax:
+                    if (condition.MinValue <= input && condition.MaxValue >= input)
+                        return condition;
+                    break;
+                case ConditionRule.EqualToMin:
+                    if (condition.MinValue <= input && condition.MaxValue > input)
+                        return condition;
+                    break;
+                case ConditionRule.EqualToMax:
+                    if (condition.MinValue < input && condition.MaxValue >= input)
+                        return condition;
+                    break;
+                default:
+                    if (condition.MinValue < input && condition.MaxValue > input)
+                        return condition;
+                    break;
+            }
+            return null;
+        }
+
+        public Condition? GetConditionResultByInput(int input)
+        {
+            Condition calculatedCondition = new Condition();
+            foreach (var condition in conditions)
+            {
+                var conditionCheck = CalculateCondition(condition, input);
+                if (conditionCheck != null)
+                {
+                    calculatedCondition = conditionCheck;
+                    break;
+                }
+            }
+            return calculatedCondition;
+        }
+    }
+    public class Condition
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+        public int ResultValue { get; set; }
+        public ConditionRule ConditionRule { get; set; }
+    }
+
+    public enum ConditionRule
+    {
+        Normal = 0, //BiggerMin and LesserMax
+        EqualToMinAndMax = 1,
+        EqualToMin = 2,
+        EqualToMax = 3
+    }
+
+}
---
+++ NetCoreTask/NetCoreTask.WindowsApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace NetCoreTask.WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sonuç";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(255, 104);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 15);
+            this.lblSonuc.TabIndex = 8;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(417, 55);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sayı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
